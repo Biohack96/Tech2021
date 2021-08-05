fa8661c2a7fc7760502000eb5ced22ce3a5350e3
cinni-99 <76706947+cinni-99@users.noreply.github.com>
2021-08-05 20:11:43 +0200

    prova home.php

---
+++ home.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+// require_once('includes/DB.php');
+// require_once('includes/create_info_utente.php');
+
+// Oggetto di accesso al database
+// $db = new DB();
+
+// Titolo della pagina
+$title = 'Share Arts';
+
+// Include i file html
+$page_head = file_get_contents('includes/head.html');
+$page_body = file_get_contents('includes/body.html');
+
+$page_head = str_replace("<titolo />", $title, $page_head);
+$page_head = str_replace("<scripts />", "", $page_head);
+
+$page_body = str_replace("<breadcrumb />", "", $page_body);  	// da aggiungere
+$page_body = str_replace("<utente />", "", $page_body);			// da aggiungere
+$page_body = str_replace("<content />", "", $page_body);		// da aggiungere
+
+// Disattiva link circolare
+$page_body = str_replace('<li><a href="home.php"><span xml:lang="en">Home</span></a></li>', '<li><span xml:lang="en">Home</span></li>', $page_body);		// da aggiungere dinamicamente
+
+
+
+echo $page_head  . $page_body  ;
+?>
