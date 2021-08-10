a8ddb2e62ca3c9e4477146d70a86a0a21f71d089
cinni-99 <76706947+cinni-99@users.noreply.github.com>
2021-08-10 22:31:23 +0200

    pagine php

---
+++ autori.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+// require_once('includes/DB.php');
+// require_once('includes/create_info_utente.php');
+
+// Oggetto di accesso al database
+// $db = new DB();
+
+// Titolo della pagina
+
+$title = 'Autori - Share Arts';
+
+// Include i file html
+$page_head = file_get_contents('includes/head.html');
+$page_body = file_get_contents('includes/body.html');
+
+$content = file_get_contents('includes/autori_list.html');
+
+$page_head = str_replace("<titolo />", $title, $page_head);
+$page_head = str_replace("<scripts />", "", $page_head);
+
+$page_body = str_replace("<breadcrumb />", "", $page_body);  	// da aggiungere
+$page_body = str_replace("<utente />", "", $page_body);			// da aggiungere
+
+// $content = str_replace("<authors />", "", $page_head);          //da sostituire con lista degli autori
+
+// Disattiva link circolare
+$page_body = str_replace('<li><a href="autori.php">Autori</a></li>', '<li>Autori</li>', $page_body);		// da aggiungere dinamicamente
+
+
+$page_body = str_replace('<content />', $content, $page_body);
+
+echo $page_head  . $page_body  ;
+?>
