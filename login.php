49bd272465a965ee8fb8d83616d4f60a31231c0a
cinni-99 <76706947+cinni-99@users.noreply.github.com>
2021-09-14 23:15:36 +0200

    validazione login

--- login.php
+++ login.php
@@ -10,7 +10,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if($db->login($_POST['username'],$_POST['password']))
     {
-        header('Location: Home.php');
+        $admin = false;
+
+        if(isset($_SESSION['user_id'])) {
+        $auth = $db->getAutoreById($_SESSION['user_id']);
+            if ($auth['isAdmin']) {
+                $admin = true;
+            }
+        }
+        if ($admin==true) {
+            header('Location: admin.php');
+        }
+        else {
+            header('Location: autori.php?id=' . $_SESSION['user_id']);
+        }
     }
     else
     $content .=  '<span class="error">Credenziali non valide</span>';
