8bfb51100a3fad8c740ad6bc25aea7fe08d3f370
Biohack <biotto.stefano@gmail.com>
2021-09-16 12:28:26 +0200

    db

--- database/db.sql
+++ database/db.sql
@@ -44,3 +44,10 @@ SET FOREIGN_KEY_CHECKS = 1; -- Riabilita check
 
 INSERT into autore(password,username,bio,isAdmin) VALUES ('04f8996da763b7a969b1028ee3007569eaf3a635486ddab211d512c85b9df8fb','user','just a user', false);
 INSERT into autore(password,username,bio,isAdmin) VALUES ('8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918','admin','admin', true);
+INSERT INTO `categoria` (`id`, `nome_categoria`, `cat_description`) VALUES
+(1, 'Quadri', 'Opere dipinte su tela'),
+(2, 'Sculture', 'Statue o rilievi'),
+(3, 'Opere architettoniche', 'Edificio o costruzioni di rilievo artistico'),
+(4, 'Graffiti', 'Opere artistiche di strada'),
+(5, 'Affreschi', 'Opere sugli intonaci interni degli edifici'),
+(6, 'Altro', 'Tutto ciò che è arte e non rientra nelle categorie presenti');
